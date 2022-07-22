646694761f8b40f2fc85f8a1f771c5e6c3ce6b7c
Rishav_Kumar <rishavkumaraug20005212@gmail.com>
2022-07-23 03:52:17 +0530

    footer logo added

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -7,10 +7,25 @@ const Footer =() =>{
         <Container
         fluid
         tag="footer"
-        className="text-center bg-primary text-white text-uppercase fixed-bottom pb-2 ">
-       
-        My project collection 
+    className="text-center bg-primary text-white text-uppercase  pb-2 ">
+    {/*className="text-center bg-primary text-white text-uppercase fixed-bottom pb-2 "> */}
+        
         {/* <audio src={music} controls  autostart="autostart" className="float-end"/> */}
+        <footer>
+        <b><p className="pr-2">Made With </p></b>
+        <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png"  id="footerlogo"/> </div> 
+        <b><p className="pl-2">and </p></b>
+        </footer>
+        
+        
+        <footer>
+        
+      <div><img src="https://git-scm.com/images/logos/downloads/Git-Logo-1788C.png"  id="footerlogo"/> </div> 
+      <div><img src="https://pngpress.com/wp-content/uploads/2020/08/uploads_github_github_PNG17.png" id="footerlogo"/></div> 
+      <div><img src="https://bulma.io/images/bulma-logo.png" id="footerlogo"/></div>
+      <div><img src="https://www.alioze.com/wp-content/uploads/2016/11/react-js.png" id="footerlogo"/></div>
+      <div><img src="https://firebase.google.com/static/downloads/brand-guidelines/PNG/logo-knockout.png" id="footerlogo"/></div>
+      </footer>
         </Container>
     );
 }
