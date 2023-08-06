028660c889c1f49f2305cbbf3de270d54a5b3b71
Rishav_Kumar <rishavkumaraug20005212@gmail.com>
2023-08-06 23:29:50 +0530

    Dead Images Link Updated

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -12,16 +12,13 @@ const Footer =() =>{
         
         {/* <audio src={music} controls  autostart="autostart" className="float-end"/> */}
         <footer>
-        <b><p className="pr-2">Made With </p></b>
-        <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png"  id="footerlogo" alt="/"/> </div> 
-        <b><p className="pl-2">and </p></b>
+        <b><p className="pr-2">Build With </p></b>
         </footer>
         
         
         <footer>
         
       <div><img src="https://git-scm.com/images/logos/downloads/Git-Logo-1788C.png" alt="/" id="footerlogo"/> </div> 
-      <div><img src="https://pngpress.com/wp-content/uploads/2020/08/uploads_github_github_PNG17.png" alt="/" id="footerlogo"/></div> 
       <div><img src="https://bulma.io/images/bulma-logo.png" id="footerlogo" alt="/"/></div>
       <div><img src="https://www.alioze.com/wp-content/uploads/2016/11/react-js.png" alt="/" id="footerlogo"/></div>
       <div><img src="https://firebase.google.com/static/downloads/brand-guidelines/PNG/logo-knockout.png" alt="/"id="footerlogo"/></div>
