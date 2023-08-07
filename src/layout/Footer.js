ba861b6928842396a3e697361b8f296cc55674f3
Rishav_Kumar <rishavkumaraug20005212@gmail.com>
2023-08-07 14:36:32 +0530

    code syntatic corrected

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,27 +1,15 @@
 import React from "react"
 import {Container} from "react-bootstrap"
-import music from "../Gallery/music.mp3"
 
 const Footer =() =>{
     return(
-        <Container
-        fluid
-        tag="footer"
-    className="text-center bg-primary text-white text-uppercase  pb-2 ">
-    {/*className="text-center bg-primary text-white text-uppercase fixed-bottom pb-2 "> */}
-        
-        {/* <audio src={music} controls  autostart="autostart" className="float-end"/> */}
-        <footer>
-        <b><p className="pr-2">Build With </p></b>
-        </footer>
-        
-        
-        <footer>
-        
-      <div><img src="https://git-scm.com/images/logos/downloads/Git-Logo-1788C.png" alt="/" id="footerlogo"/> </div> 
-      <div><img src="https://bulma.io/images/bulma-logo.png" id="footerlogo" alt="/"/></div>
-      <div><img src="https://www.alioze.com/wp-content/uploads/2016/11/react-js.png" alt="/" id="footerlogo"/></div>
-      <div><img src="https://firebase.google.com/static/downloads/brand-guidelines/PNG/logo-knockout.png" alt="/"id="footerlogo"/></div>
+        <Container  fluid tag="footer" className="text-center bg-primary text-white text-uppercase  pb-2 ">
+         <footer> <b><p className="pr-2">Build With </p></b> </footer>
+      <footer>
+       <div><img src="https://git-scm.com/images/logos/downloads/Git-Logo-1788C.png" alt="/" id="footerlogo"/> </div> 
+       <div><img src="https://bulma.io/images/bulma-logo.png" id="footerlogo" alt="/"/></div>
+       <div><img src="https://www.alioze.com/wp-content/uploads/2016/11/react-js.png" alt="/" id="footerlogo"/></div>
+       <div><img src="https://firebase.google.com/static/downloads/brand-guidelines/PNG/logo-knockout.png" alt="/"id="footerlogo"/></div>
       </footer>
         </Container>
     );
